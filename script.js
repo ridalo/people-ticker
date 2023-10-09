2c5c61c70417ed00a7863e26f01e8b8945fd251b
ridalo <ridalo1989@gmail.com>
2023-10-09 18:03:27 -0400

    problem solved

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-// // document.getElementById("count-el").innerText = 5
+document.getElementById("count-el").innerText = 5
 
-// let firstBatch = 5
-// let secondBatch = 7
-// let count = firstBatch + secondBatch
-// console.log(count)
-
-
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
+console.log(count)
 
 
+
+let myAge = 33
+
+let humanDogRatio = 7
+
+let total = myAge * humanDogRatio
+
+console.log(total)
 
 
 
