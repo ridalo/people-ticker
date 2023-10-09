1b758f5d391121ae0d704f7401b80e2cb8c0b871
ridalo <ridalo1989@gmail.com>
2023-10-08 23:03:52 -0400

    practice

---
+++ script.js
@@ -0,0 +1,13 @@
+// // document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+
+
+
+
+
+
