70ba1b95e18b60c74def14b1ba0829237b3e003d
ridalo <ridalo1989@gmail.com>
2023-10-09 22:05:57 -0400

    new challenges

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
 
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch + secondBatch
-console.log(count)
+// function increment() {
+// console.log("button was clicked")
+// }
 
+// function lifeMeans() {
+// 	console.log(42)
+// }
 
+// lifeMeans()
 
-let myAge = 33
+let lapsCompleted = 0
 
-let humanDogRatio = 7
+function incrementLap() {
+	lapsCompleted = lapsCompleted + 1
+	
+}
 
-let total = myAge * humanDogRatio
-
-console.log(total)
-
-
-
+incrementLap()
+incrementLap()
+incrementLap()
+console.log(lapsCompleted)
